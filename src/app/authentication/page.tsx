e331bfe8b40baa22d2c79cba3a0d1f0e89fd113f
FDaniloSoares <fdanilosoares@gmail.com>
2025-05-28 00:23:52 -0300

    feat: add signup

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,18 @@
+const AuthenticationPage = () => {
+  return (
+    <div>
+      <h1 className="flex h-screen w-screen items-center justify-center">
+        Authentication
+      </h1>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
+
+
+
+
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,54 @@
+"use client";
+
+import { Label } from "@radix-ui/react-label";
+
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SignUpForm from "./authentication/components/sign-up-form";
 
 export default function Home() {
-  return <Button>Home</Button>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Faça login para continuar</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" defaultValue="@peduarte" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Entrar</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
