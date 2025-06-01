2638f71d957fae8f303cda161c182a2e83700355
FDaniloSoares <fdanilosoares@gmail.com>
2025-05-31 22:51:29 -0300

    feat: ensure doctor availability is saved as utc on database

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,4 +1,6 @@
 "use server";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -8,9 +10,23 @@ import { actionClient } from "@/lib/safe-action";
 
 import { upsertDoctorSchema } from "./schema";
 
+dayjs.extend(utc);
+
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
   .action(async ({ parsedInput: data }) => {
+    const availableFromTimeUTC = dayjs()
+      .set("hour", parseInt(data.availableFromTime.split(":")[0]))
+      .set("minute", parseInt(data.availableFromTime.split(":")[1]))
+      .set("second", parseInt(data.availableFromTime.split(":")[2]))
+      .utc();
+
+    const availableToTimeUTC = dayjs()
+      .set("hour", parseInt(data.availableToTime.split(":")[0]))
+      .set("minute", parseInt(data.availableToTime.split(":")[1]))
+      .set("second", parseInt(data.availableToTime.split(":")[2]))
+      .utc();
+
     const session = await auth.api.getSession({
       headers: await headers(),
     });
@@ -26,14 +42,18 @@ export const upsertDoctor = actionClient
     await db
       .insert(doctorsTable)
       .values({
-        id: data.id,
         ...data,
+        id: data.id,
         clinicId: session?.user.clinic?.id,
+        availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+        availableToTime: availableToTimeUTC.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
         set: {
           ...data,
+          availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+          availableToTime: availableToTimeUTC.format("HH:mm:ss"),
         },
       });
   });
