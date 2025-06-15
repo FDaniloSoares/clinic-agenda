f5a1bdd0da35ef8368a7ebfcb5471eddf83b51e0
FDaniloSoares <fdanilosoares@gmail.com>
2025-06-14 22:12:11 -0300

    feat: add stripe webhook processing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const router = useRouter();
+
+  useEffect(() => {
+    router.push("/authentication");
+  }, [router]); // executa 1 vez quando o componente monta
 
   const redirectToAuthentication = () => {
     router.push("/authentication");
